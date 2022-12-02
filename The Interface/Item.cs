d1c5ac85dc30c14c108195c4160a558b3db9d450
Amir Offir <amirosoffir@gmail.com>
2022-12-02 13:17:40 +0200

    Add files via upload

---
+++ The Interface/Item.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheInterface
+{
+    internal interface Item
+    {
+        
+        int GetPrice();
+    }
+
+    public class Cola : Item
+    {
+        public int GetPrice()
+        {
+            return 11;
+        }
+    }
+    public class Kinly : Item
+    {
+        public int GetPrice()
+        {
+            return 10;
+        }
+    }
+}
